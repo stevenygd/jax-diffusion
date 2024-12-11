dd364a057b75716315cc4e78f4df4de7653b913f
ujinsong23 <ujinsong23@gmail.com>
2024-12-11 02:37:17 +0000

    remove more scripts, revise 4 config for 4 model

--- scripts/run_train.sh
+++ scripts/run_train.sh
@@ -10,20 +10,23 @@ export WANDB_PROJECT="mt3_highres"
 export WANDB_TEAM="xnf"
 
 # directory of the codebase
-export CODE_DIR=/mnt/disks/nfs/ujinsong/jax-diffusion
+CODE_DIR=/mnt/disks/nfs/ujinsong/jax-diffusion
 
 # directory that stores the checkpoints upon training
-export OUT_DIR=/mnt/disks/gs/sci-guandao
+OUT_DIR=/mnt/disks/gs/sci-guandao
 
 # directory that reads the data from
-export DATA_DIR=/mnt/disks/data
+DATA_DIR=/mnt/disks/data
 
+# model name
+MODEL_NAME=ssm_xl
+EXPR_NAME=release
 
 export LIBTPU_INIT_ARGS=" --xla_tpu_impure_oom_fast_exit_threshold=-1 --xla_tpu_enable_data_parallel_all_reduce_opt=true --xla_tpu_data_parallel_opt_different_sized_ops=true --xla_tpu_enable_async_collective_fusion=true --xla_tpu_enable_async_collective_fusion_fuse_all_gather=true --xla_tpu_enable_async_collective_fusion_multiple_steps=true --xla_tpu_overlap_compute_collective_tc=true --xla_enable_async_all_gather=true"
-XLA_PYTHON_CLIENT_MEM_FRACTION=.95 PYTHONPATH=${CODE_DIR} WANDB_DIR=${WANDB_DIR} python ${CODE_DIR}/train.py \
+XLA_PYTHON_CLIENT_MEM_FRACTION=.95 PYTHONPATH=${CODE_DIR} python ${CODE_DIR}/train.py \
   --config-name config \
-  model=dit_l \
-  expr_name="release/" \
+  model=${MODEL_NAME} \
+  expr_name=${EXPR_NAME} \
   data_dir=${DATA_DIR} \
   output_dir=${OUT_DIR} \
   hydra_dir=${HYDRA_DIR} \
